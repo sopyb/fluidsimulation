0bb058f1ba82dc4a186743648f10ccf750f0b335
sopy <doimpt@sopy.one>
2022-12-03 22:58:17 +0200

    fluidSim: comments completed and functions reordered

--- fluidSim.js
+++ fluidSim.js
@@ -1,6 +1,11 @@
 let iterations = 4
 
 class FluidBox {
+  /**
+   * @description Creates the fluid box object
+   * @param {Number} size
+   * @param {FluidBox} oldFluidBox
+   */
   constructor (size, oldFluidBox) {
     this.size = size // size of the box
 
@@ -26,6 +31,94 @@ class FluidBox {
     if (oldFluidBox) this.import(oldFluidBox)
   }
 
+  /*********************************************\
+   *  Code for interacting with the fluid box  *
+  \*********************************************/
+
+  /**
+   * @description Add color to the box.
+   * @param {Number} i - x coordinate
+   * @param {Number} j - y coordinate
+   * @param {Array<Number>} color - color to add
+   */
+  addColor (i, j, color) {
+    // get the index
+    let index = this.index(i, j)
+
+    // add color
+    this.color[0][index] += color[0]
+    this.color[1][index] += color[1]
+    this.color[2][index] += color[2]
+  }
+
+  /**
+   * @description Adds velocity to the box.
+   * @param {Number} i - x coordinate
+   * @param {Number} j - y coordinate
+   * @param {Number} amountX - amount of velocity in the x direction
+   * @param {Number} amountY - amount of velocity in the y direction
+   */
+  addVelocity (i, j, amountX, amountY) {
+    // add velocity to the box
+
+    // get the index
+    let index = this.index(i, j)
+
+    // add velocity
+    this.velocity[0][index] += amountX * settings.velocityMultiplier
+    this.velocity[1][index] += amountY * settings.velocityMultiplier
+  }
+
+  /**
+   * @description Get the density at a point
+   * @param {Number} i - index
+   * @returns {Array<Number>}
+   */
+  getDensity (i) {
+    // return RGB array mapping each element color to a value e = e[i] f0r each color channel
+    return this.color.map(x => x[i])
+  }
+
+  /**
+   * @description Steps the simulation forward.
+   */
+  step () {
+    // diffuse velocity
+    this.diffuse(1, this.velocity0[0], this.velocity[0], settings.viscosity)
+    this.diffuse(2, this.velocity0[1], this.velocity[1], settings.viscosity)
+
+    // project velocity to make it divergence free
+    this.project(this.velocity0[0], this.velocity0[1], this.velocity[0], this.velocity[1])
+
+    // advect velocity - move velocity to the next step
+    this.advect(1, this.velocity[0], this.velocity0[0], this.velocity0[0], this.velocity0[1])
+    this.advect(2, this.velocity[1], this.velocity0[1], this.velocity0[0], this.velocity0[1])
+
+    // get rid of the divergence
+    this.project(this.velocity[0], this.velocity[1], this.velocity0[0], this.velocity0[1])
+
+    // diffuse the color
+    this.diffuse(0, this.color0[0], this.color[0], settings.diffusion)
+    this.diffuse(0, this.color0[1], this.color[1], settings.diffusion)
+    this.diffuse(0, this.color0[2], this.color[2], settings.diffusion)
+
+    // advect the color - move color to the next step
+    this.advect(0, this.color[0], this.color0[0], this.velocity[0], this.velocity[1])
+    this.advect(0, this.color[1], this.color0[1], this.velocity[0], this.velocity[1])
+    this.advect(0, this.color[2], this.color0[2], this.velocity[0], this.velocity[1])
+
+    // fade out
+    this.fadeOut()
+  }
+
+  /*********************************************\
+   *          Code for the simulation          *
+  \*********************************************/
+
+  /**
+   * @description import old fluid box to the new one to make the simulation continuous
+   * @param {FluidBox} oldFluidBox
+   */
   import (oldFluidBox) {
     if (this.size < oldFluidBox.size) {
       // super sampling
@@ -85,105 +178,168 @@ class FluidBox {
     }
   }
 
+  /**
+   * @description Get the index of a cell
+   * @param {Number} i - x coordinate
+   * @param {Number} j - y coordinate
+   * @returns {Number} - index
+   */
   index (i, j) {
+    // check if i and j are in the box
     if (i < 0) i = 0
     if (i > this.size - 1) i = this.size - 1
     if (j < 0) j = 0
     if (j > this.size - 1) j = this.size - 1
 
+    // return index
     return i + j * this.size
   }
 
-  addColor (i, j, color) {
-    let index = this.index(i, j)
-    this.color[0][index] += color[0]
-    this.color[1][index] += color[1]
-    this.color[2][index] += color[2]
-  }
+  /**
+   * @description Advection step of the simulation - move the fluid to the next step
+   * @param b - boundary condition
+   * @param d - density
+   * @param d0 - old density
+   * @param vX - x velocity
+   * @param vY - y velocity
+   */
+  advect (b, d, d0, vX, vY) {
+    // calculate the time step
+    let dtx = settings.dt * (this.size - 2)
+    let dty = settings.dt * (this.size - 2)
 
-  addDensity (i, j, color) {
-    let index = this.index(i, j)
+    // variables for the interpolation
+    let i0, i1, j0, j1   // the four corners of the cell
+    let s0, s1, t0, t1   // the interpolation weights
+    let tmp1, tmp2, x, y // temporary variables
 
-    color = color.map(x => Math.floor(x * settings.density / 255))
+    // iterate over all cells
+    for (let j = 1; j < this.size - 1; j++) {
+      for (let i = 1; i < this.size - 1; i++) {
+        // calculate the position of the cell
+        tmp1 = dtx * vX[this.index(i, j)]
+        tmp2 = dty * vY[this.index(i, j)]
 
-    this.color[0][index] += color[0]
-    this.color[1][index] += color[1]
-    this.color[2][index] += color[2]
-  }
+        // calculate the position of the cell in the previous frame
+        x = i - tmp1
+        y = j - tmp2
 
-  getDensity (i) {
-    return this.color.map(x => x[i])
-  }
+        // clamp the position
+        // clamp the x position
+        if (x < 0.5) x = 0.5
+        if (x > this.size + 0.5) x = this.size + 0.5
+        i0 = Math.floor(x)
+        i1 = i0 + 1.0
 
-  addVelocity (i, j, amountX, amountY) {
-    let index = this.index(i, j)
-    this.velocity[0][index] += amountX * settings.velocityMultiplier
-    this.velocity[1][index] += amountY * settings.velocityMultiplier
-  }
+        // clamp the y position
+        if (y < 0.5) y = 0.5
+        if (y > this.size + 0.5) y = this.size + 0.5
+        j0 = Math.floor(y)
+        j1 = j0 + 1.0
 
-  step () {
-    // diffuse velocity
-    this.diffuse(1, this.velocity0[0], this.velocity[0], settings.viscosity)
-    this.diffuse(2, this.velocity0[1], this.velocity[1], settings.viscosity)
+        // calculate the interpolation weights
+        s1 = x - i0
+        s0 = 1.0 - s1
+        t1 = y - j0
+        t0 = 1.0 - t1
 
-    // project velocity to make it divergence free
-    this.project(this.velocity0[0], this.velocity0[1], this.velocity[0], this.velocity[1])
+        // interpolate the value
+        let i0i = i0
+        let i1i = i1
+        let j0i = j0
+        let j1i = j1
 
-    // advect velocity - move velocity to the next step
-    this.advect(1, this.velocity[0], this.velocity0[0], this.velocity0[0], this.velocity0[1])
-    this.advect(2, this.velocity[1], this.velocity0[1], this.velocity0[0], this.velocity0[1])
-
-    // get rid of the divergence
-    this.project(this.velocity[0], this.velocity[1], this.velocity0[0], this.velocity0[1])
-
-    // diffuse the color
-    this.diffuse(0, this.color0[0], this.color[0], settings.diffusion)
-    this.diffuse(0, this.color0[1], this.color[1], settings.diffusion)
-    this.diffuse(0, this.color0[2], this.color[2], settings.diffusion)
-
-    // advect the color - move color to the next step
-    this.advect(0, this.color[0], this.color0[0], this.velocity[0], this.velocity[1])
-    this.advect(0, this.color[1], this.color0[1], this.velocity[0], this.velocity[1])
-    this.advect(0, this.color[2], this.color0[2], this.velocity[0], this.velocity[1])
-
-    // fade out
-    this.fadeOut()
-  }
-
-  fadeOut () {
-    for (let i = 0; i < this.size * this.size; i++) {
-      let fade = 1 - settings.fadeout * settings.dt
-      // fade out relative to dt
-      this.color[0][i] *= fade
-      this.color[1][i] *= fade
-      this.color[2][i] *= fade
+        // set the value
+        d[this.index(i, j)] =
+          s0 * (t0 * d0[this.index(i0i, j0i)] + t1 * d0[this.index(i0i, j1i)]) +
+          s1 * (t0 * d0[this.index(i1i, j0i)] + t1 * d0[this.index(i1i, j1i)])
+      }
     }
+    // set the boundary conditions
+    this.setBoundary(b, d)
   }
 
-  // diffuse - make the x spread out inside the box
+  /**
+   * @description Diffusion step of the simulation - diffuse the fluid
+   * @param b - boundary condition
+   * @param x - density
+   * @param x0 - old density
+   * @param diff - diffusion rate
+   */
   diffuse (b, x, x0, diff) {
-    // diffuse the array x with the array x0
-    // b is the boundary condition
-    // diff is the diffusion rate
-
     // calculate the diffusion rate
     let a = settings.dt * diff * (this.size - 2) * (this.size - 2)
     // pass the diffusion rate to the linear solver
     this.linearSolve(b, x, x0, a, 1 + 4 * a)
   }
 
-  // linear solve - solves the equation Ax = b
-  linearSolve (b, x, x0, a, c) {
-    // we need to solve Ax = b
-    // where A is a tridiagonal matrix
-    // and x and b are vectors
-    // and x0 is the previous x
-    // and a and c are constants representing the matrix
-    // we use Gauss-Seidel method
-    // https://en.wikipedia.org/wiki/Gauss%E2%80%93Seidel_method
-    // https://en.wikipedia.org/wiki/Tridiagonal_matrix_algorithm
-    // https://en.wikipedia.org/wiki/Tridiagonal_matrix
+  /**
+   * @description Projection step of the simulation - project the velocity field
+   * @param vX  - x velocity
+   * @param vY  - y velocity
+   * @param p   - pressure
+   * @param div - divergence
+   */
+  project (vX, vY, p, div) {
+    // calculate the divergence
+    for (let j = 1; j < this.size - 1; j++) {
+      for (let i = 1; i < this.size - 1; i++) {
+        div[this.index(i, j)] = -0.5 * (vX[this.index(i + 1, j)] - vX[this.index(i - 1, j)] + vY[this.index(i, j + 1)] - vY[this.index(i, j - 1)]) / this.size
+        p[this.index(i, j)] = 0
+      }
+    }
 
+    // set the boundary conditions
+    this.setBoundary(0, div)
+    this.setBoundary(0, p)
+
+    // solve the pressure
+    this.linearSolve(0, p, div, 1, 4)
+
+    // subtract the gradient of the pressure from the velocity
+    for (let j = 1; j < this.size - 1; j++) {
+      for (let i = 1; i < this.size - 1; i++) {
+        vX[this.index(i, j)] -= 0.5 * (p[this.index(i + 1, j)] - p[this.index(i - 1, j)]) * this.size
+        vY[this.index(i, j)] -= 0.5 * (p[this.index(i, j + 1)] - p[this.index(i, j - 1)]) * this.size
+      }
+    }
+
+    // set the boundary conditions
+    this.setBoundary(1, vX)
+    this.setBoundary(2, vY)
+  }
+
+  /**
+   * @description Fades the density of the fluid to black over time
+   */
+  fadeOut () {
+    // fade out the color
+
+    // get the fade out factor
+    let fade = 1 - settings.fadeout * settings.dt
+
+    // fade out for each cell
+    for (let i = 0; i < this.size * this.size; i++) {
+      // fade out each color channel
+      this.color[0][i] *= fade
+      this.color[1][i] *= fade
+      this.color[2][i] *= fade
+    }
+  }
+
+  /**
+   * @description Linear solver for the diffusion and projection step
+   * @param b  - boundary condition
+   * @param x  - value
+   * @param x0 - value from the previous step
+   * @param a  - constant a
+   * @param c  - constant c
+   *
+   * https://en.wikipedia.org/wiki/Gauss%E2%80%93Seidel_method
+   * https://en.wikipedia.org/wiki/Tridiagonal_matrix_algorithm
+   * https://en.wikipedia.org/wiki/Tridiagonal_matrix
+   */
+  linearSolve (b, x, x0, a, c) {
     // reciprocal of c
     let cRecip = 1.0 / c
 
@@ -209,92 +365,28 @@ class FluidBox {
     }
   }
 
-  // set boundary - Check fluid doesn't escape the box by mirroring the values
+  /**
+   * @description Boundary conditions for the simulation
+   * @param b - boundary condition
+   * @param x - value
+   */
   setBoundary (b, x) {
-    // boundary conditions
-    // b is the boundary condition
-    // x is the array to apply the boundary conditions to
-
+    // set the boundary conditions for the array x
     for (let i = 1; i < this.size - 1; i++) {
       x[this.index(i, 0)] = b === 2 ? -x[this.index(i, 1)] : x[this.index(i, 1)]
       x[this.index(i, this.size - 1)] = b === 2 ? -x[this.index(i, this.size - 2)] : x[this.index(i, this.size - 2)]
     }
 
+    // set the boundary conditions for the array x
     for (let j = 1; j < this.size - 1; j++) {
       x[this.index(0, j)] = b === 1 ? -x[this.index(1, j)] : x[this.index(1, j)]
       x[this.index(this.size - 1, j)] = b === 1 ? -x[this.index(this.size - 2, j)] : x[this.index(this.size - 2, j)]
     }
 
+    // set the boundary conditions for the array x
     x[this.index(0, 0)] = 0.5 * (x[this.index(1, 0)] + x[this.index(0, 1)])
     x[this.index(0, this.size - 1)] = 0.5 * (x[this.index(1, this.size - 1)] + x[this.index(0, this.size - 2)])
     x[this.index(this.size - 1, 0)] = 0.5 * (x[this.index(this.size - 2, 0)] + x[this.index(this.size - 1, 1)])
     x[this.index(this.size - 1, this.size - 1)] = 0.5 * (x[this.index(this.size - 2, this.size - 1)] + x[this.index(this.size - 1, this.size - 2)])
-  }
-
-  project (vX, vY, p, div) {
-    for (let j = 1; j < this.size - 1; j++) {
-      for (let i = 1; i < this.size - 1; i++) {
-        div[this.index(i, j)] = -0.5 * (vX[this.index(i + 1, j)] - vX[this.index(i - 1, j)] + vY[this.index(i, j + 1)] - vY[this.index(i, j - 1)]) / this.size
-        p[this.index(i, j)] = 0
-      }
-    }
-    this.setBoundary(0, div)
-    this.setBoundary(0, p)
-    this.linearSolve(0, p, div, 1, 4)
-
-    for (let j = 1; j < this.size - 1; j++) {
-      for (let i = 1; i < this.size - 1; i++) {
-        vX[this.index(i, j)] -= 0.5 * (p[this.index(i + 1, j)] - p[this.index(i - 1, j)]) * this.size
-        vY[this.index(i, j)] -= 0.5 * (p[this.index(i, j + 1)] - p[this.index(i, j - 1)]) * this.size
-      }
-    }
-    this.setBoundary(1, vX)
-    this.setBoundary(2, vY)
-  }
-
-  advect (b, d, d0, vX, vY) {
-    let i0, i1, j0, j1
-
-    let dtx = settings.dt * (this.size - 2)
-    let dty = settings.dt * (this.size - 2)
-
-    let s0, s1, t0, t1
-    let tmp1, tmp2, x, y
-
-    let i, j
-
-    for (j = 1; j < this.size - 1; j++) {
-      for (i = 1; i < this.size - 1; i++) {
-        tmp1 = dtx * vX[this.index(i, j)]
-        tmp2 = dty * vY[this.index(i, j)]
-        x = i - tmp1
-        y = j - tmp2
-
-        if (x < 0.5) x = 0.5
-        if (x > this.size + 0.5) x = this.size + 0.5
-        i0 = Math.floor(x)
-        i1 = i0 + 1.0
-
-        if (y < 0.5) y = 0.5
-        if (y > this.size + 0.5) y = this.size + 0.5
-        j0 = Math.floor(y)
-        j1 = j0 + 1.0
-
-        s1 = x - i0
-        s0 = 1.0 - s1
-        t1 = y - j0
-        t0 = 1.0 - t1
-
-        let i0i = i0
-        let i1i = i1
-        let j0i = j0
-        let j1i = j1
-
-        d[this.index(i, j)] =
-          s0 * (t0 * d0[this.index(i0i, j0i)] + t1 * d0[this.index(i0i, j1i)]) +
-          s1 * (t0 * d0[this.index(i1i, j0i)] + t1 * d0[this.index(i1i, j1i)])
-      }
-    }
-    this.setBoundary(b, d)
   }
 }
